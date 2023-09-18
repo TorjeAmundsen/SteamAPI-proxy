13fb1a3270298cc7da1fb63310e2ddb6eba3c55f
Torje Amundsen <torjefrafen@hotmail.no>
2023-09-18 10:42:18 +0200

    added cors

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import axios from "axios";
+import cors from "cors";
 import "dotenv/config";
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res("KF2 Tracker");
@@ -11,9 +14,6 @@ app.get("/", (req, res) => {
 
 app.get("/:steamId", async (req, res) => {
   try {
-    console.log(
-      `https://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=232090&key=${process.env.STEAM_API_KEY}&steamid=${req.params.steamId}/`
-    );
     const response = await axios.get(
       `https://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=232090&key=${process.env.STEAM_API_KEY}&steamid=${req.params.steamId}/`
     );
